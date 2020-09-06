8dd129d1ab00168941cdc5bad8c2a0d61d2f0af8
Leah Winters <leah.winters24@gmail.com>
2020-09-06 13:55:18 -0600

    Add hard code of destination cards and add functioning carousel

--- src/Destinations/Destinations.js
+++ src/Destinations/Destinations.js
@@ -1,15 +1,32 @@
 import React, { useState }  from 'react';
 import './Destinations.css';
+import Slider from "react-slick";
 import DestinationCard from '../DestinationCard/DestinationCard';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Destinations() {
+  const settings = {
+    // className: "center",
+    centerMode: true,
+    centerPadding: '0',
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+  };
+
   return (
     <div className='destinations'>
-      <DestinationCard />
-      <DestinationCard />
-      <DestinationCard />
-      <DestinationCard />
+      <Slider  {...settings}>
+        <DestinationCard />
+        <DestinationCard />
+        <DestinationCard />
+        <DestinationCard />
+      </Slider>
     </div>
+    
   )
 }
 
