714e6c874fc0a0a299be74f49bd85adb735d32b9
John Watterson <john@breakthrewbrew.com>
2020-09-07 13:22:53 -0600

    :crystal_ball: Add hard coded icon to the destination card
    Co-authored-by: Liliana Weimer <lilianaweimer11@gmail.com>
    Co-authored-by: Leah Winters <leah.winters24@gmail.com>”

--- src/Components/DestinationCard/DestinationCard.js
+++ src/Components/DestinationCard/DestinationCard.js
@@ -1,16 +1,22 @@
-import React, { useState }  from 'react';
-import './DestinationCard.css';
+import React, { useState } from "react";
+import "./DestinationCard.css";
 
 function DestinationCard() {
   return (
-    <div className='destination-card'>
-      <p className='destination-name'>Destination Name & Icon</p>
+    <div className="destination-card">
+      <section className="destination-name-and-icon">
+        <p className="destination-name">Destination Name</p>
+        <img
+          className="destination-icon"
+          src={require("../../Images/moon.svg")}
+        />
+      </section>
       <p>Destination Image</p>
-      <button className='plan-voyage-btn' role='button'>
+      <button className="plan-voyage-btn" role="button">
         Plan My Voyage!
       </button>
     </div>
-  )
+  );
 }
 
 export default DestinationCard;
