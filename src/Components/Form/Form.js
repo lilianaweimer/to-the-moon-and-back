8b15bca7d70fc0fb7b47646d2441ba17a4876137
Liliana Weimer <lilianaweimer11@gmail.com>
2020-09-07 15:36:17 -0600

    :crystal_ball: Conditionally render correct number of passenger forms and submit button
    Co-authored-by: Leah Winters <leah.winters24@gmail.com>
    Co-authored-by: John Watterson <John@breakthrewbrew.com>

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Form.css"
 
 const Form = () => {
 
+  const [days, setDays] = useState("");
+  const [travelerCount, setTravelerCount] = useState(0);
+  const [travelers, setTravelers] = useState([]);
+
+  const showTravelerForms = () => {
+    let dumpster = []
+    for (let i = 0; i < travelerCount; i++) {
+        dumpster.push(i)
+    }
+    return dumpster.map(dumpsterNum => {
+      return (
+        <div key={dumpsterNum}>
+          <p>Traveler {dumpsterNum + 1}</p>
+          <label>
+            Name:
+            <input 
+              type="text" 
+              name="name"
+              placeholder="Name"
+              onChange={() => {}}
+            />
+          </label>
+          <br />
+          <label>
+            Weight (lbs):
+            <input 
+              type="text" 
+              name="weight"
+              placeholder="Weight (lbs)"
+              onChange={() => {}}
+            />
+          </label>
+          <br />
+          <label>
+            Age:
+            <input 
+              type="text"
+              name="age"
+              placeholder="Age"
+              onChange={() => {}}
+            />
+          </label>
+        </div>
+      )
+    })
+
+  }
 
   return (
-    <fieldset className='form-container'>
+    <fieldset className="form-container">
       <legend>Voyage Planner:</legend>
-      <form className='booking-form'>
+      <form className="booking-form">
         <label>
           How many Earth days do you want to spend at [destination]?
           <br />
           <input 
-            type='number' 
-            placeholder='Earth Days' 
-            name='days'
+            type="number" 
+            placeholder="Earth Days" 
+            name="days"
+            onChange={event => setDays(event.target.value)}
           />
         </label>
         <br />
@@ -22,17 +70,22 @@ const Form = () => {
           How many people will be on your voyage?
           <br />
           <select 
-            alt='Number of Travelers' 
-            name='travelers' 
-            className='dropdown'
+            alt="Number of Travelers" 
+            name="traveler-count" 
+            className="dropdown"
+            onChange={event => setTravelerCount(event.target.value)}
           >
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
         </select>
         </label>
+        {showTravelerForms()}
+        <br />
+        <button disabled={true}>Start My Voyage!</button>
       </form>
     </fieldset>
   )
