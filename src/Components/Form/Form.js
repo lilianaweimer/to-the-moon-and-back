5fc685224b1853353b8f97dbcd0236d1fa0727f4
Liliana Weimer <lilianaweimer11@gmail.com>
2020-09-07 14:37:51 -0600

    :crystal_ball: Add basic inputs and dropdowns to Form component
    Co-authored-by: Leah Winters <leah.winters24@gmail.com>
    Co-authored-by: John Watterson <John@breakthrewbrew.com>

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 const Form = () => {
+
+
   return (
-    <p>this will become a form</p>
+    <fieldset>
+      <form>
+        <label for='days'>How many Earth days do you want to spend at [destination]?</label>
+        <input 
+          type='number' 
+          placeholder='Earth Days' 
+          name='days'
+        />
+        <label for='travelers'>How many people will be on your voyage?</label>
+        <select 
+          alt='Number of Travelers' 
+          name='travelers' 
+          className='dropdown'
+        >
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+        </select>
+      </form>
+    </fieldset>
   )
 }
 
