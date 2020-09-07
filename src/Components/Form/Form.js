21231fbb42fc19cb6555447a0c89c71d495e57f3
Liliana Weimer <lilianaweimer11@gmail.com>
2020-09-07 14:45:59 -0600

    :art: Add basic styling to Form
    Co-authored-by: John Watterson <John@breakthrewbrew.com>
    Co-authored-by: Leah Winters <leah.winters24@gmail.com>

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,29 +1,38 @@
 import React from 'react';
+import "./Form.css"
 
 const Form = () => {
 
 
   return (
-    <fieldset>
-      <form>
-        <label for='days'>How many Earth days do you want to spend at [destination]?</label>
-        <input 
-          type='number' 
-          placeholder='Earth Days' 
-          name='days'
-        />
-        <label for='travelers'>How many people will be on your voyage?</label>
-        <select 
-          alt='Number of Travelers' 
-          name='travelers' 
-          className='dropdown'
-        >
-          <option value='1'>1</option>
-          <option value='2'>2</option>
-          <option value='3'>3</option>
-          <option value='4'>4</option>
-          <option value='5'>5</option>
+    <fieldset className='form-container'>
+      <legend>Voyage Planner:</legend>
+      <form className='booking-form'>
+        <label>
+          How many Earth days do you want to spend at [destination]?
+          <br />
+          <input 
+            type='number' 
+            placeholder='Earth Days' 
+            name='days'
+          />
+        </label>
+        <br />
+        <label>
+          How many people will be on your voyage?
+          <br />
+          <select 
+            alt='Number of Travelers' 
+            name='travelers' 
+            className='dropdown'
+          >
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
         </select>
+        </label>
       </form>
     </fieldset>
   )
