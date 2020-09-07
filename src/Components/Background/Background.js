584005c9eb443cf125484ce933a617a227f071d2
John Watterson <john@breakthrewbrew.com>
2020-09-07 14:02:47 -0600

    :art: Change the homepage div dimensions and chnage the animation keyframes speed
    Co-authored-by: Liliana Weimer <lilianaweimer11@gmail.com>
    Co-authored-by: Leah Winters <leah.winters24@gmail.com>”

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -1,16 +1,20 @@
-import React, { useState }  from 'react';
-import './Background.scss';
+import React, { useState, PureComponent } from "react";
+import "./Background.scss";
 
-function Background() {
-  return (
-    <>
-    <div id="stars"></div>
-    <div id="stars2"></div>
-    <div id="stars3"></div>
-    </>
-  )
+class Background extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="background">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+      </section>
+    );
+  }
 }
 
-export default Background
-
-
+export default Background;
