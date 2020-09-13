914c2a6446325a76f68cfb4ee9d0dba2337ef648
Leah Winters <leah.winters24@gmail.com>
2020-09-13 17:23:37 -0600

    :crystal_ball: :art: Display passenger info and destination info with minial styling
    Co-authored-by: Liliana Weimer <lilianaweimer11@gmail.com>

--- src/Components/LandingSite/LandingSite.js
+++ src/Components/LandingSite/LandingSite.js
@@ -22,6 +22,34 @@ const LandingSite = ({ setTravelingState, destination, passengers }) => {
     })
   }
 
+  const displayPassengerInfo = () => {
+    return passengers.map(passenger => {
+      return (
+        <div key={passenger.id} className="passenger-info">
+          <p>Name: {passenger.name}</p>
+          <p>Weight On Earth: {passenger.weight} lbs</p>
+          <p>Weight at {destination.name}: {Number(passenger.weight) * Number(destination.gravity) } lbs</p>
+          <p>Age on Earth: {passenger.age}</p>
+          <p>Age at {destination.name}: {Number(passenger.age) + (destination.travel.travel_time / 8760)}</p>
+        </div>
+      )
+    })
+  }
+
+  const displayDestinationInfo = () => {
+    return (
+      <div className="">
+        <p>Location: {destination.name}</p>
+        <p>Location Type: {destination.celestial_body_type}</p>
+        <p>Gravity: {destination.gravity}</p>
+        <p>Planet Day: {destination.planet_day}</p>
+        <p>Planet Year: {destination.planet_year}</p>
+        <p>Travel Distance: {destination.travel.distance}</p>
+        <p>Travel Time: {destination.travel.travel_time}</p>
+      </div>
+    )
+  }
+
   useEffect(() => {
     getLandMarks(destination.id)
     .then(data => setLandMarks(data));
@@ -44,11 +72,11 @@ const LandingSite = ({ setTravelingState, destination, passengers }) => {
     <div className="landing-container">
       <div className="landing-img-info">
         <img className="destination-landing-img" src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,h_800,f_auto/Nasa_Hi-res_neptune_despina_transit_combo_despinabrightened_usbm0u.jpg" alt="destination"/>
-        <section className="destination-info">
-          <p>kdjsdlkjlkads</p>
+        <section className="destination-info-container">
+          {displayDestinationInfo()}
         </section>
-        <section className="travelers-info">
-          <p>kdjsdlkjlkads</p>
+        <section className="traveler-info-container">
+          {displayPassengerInfo()}
         </section>
       </div>
       <div className="land-marks-container">
