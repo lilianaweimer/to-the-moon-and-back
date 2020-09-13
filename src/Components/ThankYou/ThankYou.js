c46b854b485a41badacc08ae6d7c3e86008cdba9
Leah Winters <leah.winters24@gmail.com>
2020-09-13 13:34:10 -0600

    :crystal_ball: Add link to button to take user back to the home page
    Co-authored-by: Liliana Weimer <lilianaweimer11@gmail.com>

--- src/Components/ThankYou/ThankYou.js
+++ src/Components/ThankYou/ThankYou.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ThankYou.css";
 
 function ThankYou({ selectedDestination }) {
   return (
     <div className="thank-you-div">
       <p className="thank-you-message">Thank you for flying with To The Moon And Back! We hope you enjoyed your trip to {selectedDestination.name} and we hope to travel with you again!</p>
-      <button className="back-to-earth-btn">Go On Another Voyage!</button>
+      <Link to="/">
+        <button className="back-to-earth-btn">Go On Another Voyage!</button>
+      </Link>
     </div>
   )
 }
