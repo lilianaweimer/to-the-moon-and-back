e96f2e16562b2148a0d22897ec3206a6e77aaa84
Leah Winters <leah.winters24@gmail.com>
2020-09-13 14:18:55 -0600

    :crystal_ball: Add functionality to display the traveling animation when user clicks to go back to earth
    Co-authored-by: Liliana Weimer <lilianaweimer11@gmail.com>

--- src/Components/ThankYou/ThankYou.js
+++ src/Components/ThankYou/ThankYou.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./ThankYou.css";
+import Hyperspace from '../Hyperspace/Hyperspace';
 
 function ThankYou({ selectedDestination }) {
+
+  const [isInHyperspace, toggleHyperspace] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => toggleHyperspace(false), 6000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
+    isInHyperspace ? 
+    <Hyperspace /> :
     <div className="thank-you-div">
       <p className="thank-you-message">Thank you for flying with To The Moon And Back! We hope you enjoyed your trip to {selectedDestination.name} and we hope to travel with you again!</p>
       <Link to="/">
