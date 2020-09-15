0352892613137eab66f0e26d00f0291cef5c9b89
Leah Winters <leah.winters24@gmail.com>
2020-09-15 13:21:35 -0600

    :bar_chart: Pass test to ensure weather component renders correctly

---
+++ src/Components/ThankYou/ThankYou.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ThankYou from "./ThankYou";
+import { MemoryRouter } from "react-router-dom";
+
+describe("ThankYou", () => {
+  let mockSelectedDestination;
+
+  beforeEach(() => {
+    mockSelectedDestination = {
+      celestial_body_type: "Star",
+      gravity: 27.95,
+      id: 9,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRkKZ8nFulIMZAK8MKI8kzvsfGnaa3YlqMMRA&usqp=CAU",
+      name: "Sun",
+      planet_day: 25.38,
+      planet_year: null,
+    };
+  });
+
+  it.skip("Should display a rocket image, thank you message, and a button, if there is a selected destination", () => {
+    const mockSetTravelingState = jest.fn();
+
+    const { getByText, getByAltText } = render(
+      <MemoryRouter>
+        <ThankYou 
+          selectedDestination={mockSelectedDestination}
+          setTravelingState={mockSetTravelingState}
+        />
+      </MemoryRouter>
+    );
+    
+    const rocket = getByAltText("rocket");
+    const message = getByText("Thank you for", {exact: false});
+    const button = getByText("Go On Another Voyage");
+
+    expect(rocket).toBeInTheDocument();
+    expect(message).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+
+  });
+});
--- src/Components/Weather/Weather.test.js
+++ src/Components/Weather/Weather.test.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Weather from './Weather';
-import { MemoryRouter } from 'react-router-dom';
+import React from "react";
+import { render } from "@testing-library/react";
+import Weather from "./Weather";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Weather", () => {
-  it("Should render a fun fact about space", async () => {
+  let mockArticle;
+
+  beforeEach(() => {
+    mockArticle = {
+      _id: "5f60ff8496c53b1fb549820a",
+      categories: [],
+      date_added: 1599983564,
+      date_published: 1600192276,
+      events: [],
+      featured: false,
+      featured_image:
+        "https://spacenews.com/wp-content/uploads/2014/12/sn-social.jpg",
+      id: "",
+      imported_date: "2020-09-13T07:52:44.713Z",
+      launches: [],
+      ll: [],
+      news_site: "spacenews",
+      news_site_long: "SpaceNews",
+      published_date: "2020-09-15T17:51:16.000Z",
+      tags: [],
+      title: "is seeking experienced journalist to join our team",
+      url:
+        "https://spacenews.com/spacenews-is-seeking-experienced-journalist-to-join-our-team/",
+    };
+  });
+
+  it("Should render a fun article about space", async () => {
     const { getByText } = render(
       <MemoryRouter>
-        <Weather article={{}}/>
+        <Weather article={mockArticle} />
       </MemoryRouter>
     );
 
-    const funFact = getByText("Recent Space News");
+    const containerTitle = getByText("Recent Space News");
+    const button = getByText("Learn More");
+    const articleTitle = getByText("is seeking", {exact: false});
 
-    expect(funFact).toBeInTheDocument();
+    expect(containerTitle).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(articleTitle).toBeInTheDocument();
   });
-})
+});
